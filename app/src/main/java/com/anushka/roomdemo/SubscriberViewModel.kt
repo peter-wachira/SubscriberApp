aa2b8ba642b365b750ecfdd95f5c329a7adc8e2a
PeteHack <pwachira900@gmail.com>
2020-11-24 11:27:50 +0300

    create subscriber  view model class

---
+++ app/src/main/java/com/anushka/roomdemo/SubscriberViewModel.kt
@@ -0,0 +1,68 @@
+package com.anushka.roomdemo
+
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.anushka.roomdemo.db.Subscriber
+import com.anushka.roomdemo.db.SubscriberRepository
+import kotlinx.coroutines.launch
+
+
+class SubscriberViewModel(private val repository: SubscriberRepository) : ViewModel(),Observable {
+
+    val subscribers = repository.subscribers
+
+    @Bindable
+    val inputName = MutableLiveData<String>()
+    @Bindable
+    val inputEmail = MutableLiveData<String>()
+    @Bindable
+    val saveOrUpdateButtonText = MutableLiveData<String>()
+    @Bindable
+    val clearAllOrDeleteButtonText = MutableLiveData<String>()
+
+    init {
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear All"
+    }
+
+    fun saveOrUpdate(){
+        val name = inputName.value!!
+        val email = inputEmail.value!!
+        insert(Subscriber(0,name,email))
+        inputName.value = null
+        inputEmail.value = null
+    }
+
+    fun clearAllOrDelete(){
+      clearAll()
+    }
+
+    fun insert(subscriber: Subscriber)= viewModelScope.launch {
+            repository.insert(subscriber)
+        }
+
+    fun update(subscriber: Subscriber) = viewModelScope.launch {
+        repository.update(subscriber)
+    }
+
+    fun delete(subscriber: Subscriber) = viewModelScope.launch {
+        repository.delete(subscriber)
+    }
+
+    fun clearAll()=viewModelScope.launch {
+        repository.deleteAll()
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
+    }
+
+
+}
