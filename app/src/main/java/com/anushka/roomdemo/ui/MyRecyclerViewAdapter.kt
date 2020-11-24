e0ab45336a79dd2f0720fe07c92c87b6ddf7a796
PeteHack <pwachira900@gmail.com>
2020-11-24 14:50:17 +0300

    create listeners for list item clicked

--- app/src/main/java/com/anushka/roomdemo/ui/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/anushka/roomdemo/ui/MyRecyclerViewAdapter.kt
@@ -9,10 +9,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.anushka.roomdemo.R
 import com.anushka.roomdemo.databinding.ListItemBinding
 import com.anushka.roomdemo.db.Subscriber
+import com.anushka.roomdemo.generated.callback.OnClickListener
 import kotlinx.android.synthetic.main.list_item.view.*
 
 
-class MyRecyclerViewAdapter (private  val subscriberList: List<Subscriber>) :RecyclerView.Adapter<MyViewHolder>(){
+class MyRecyclerViewAdapter (private  val subscriberList: List<Subscriber>,private  val clickListener: (Subscriber)->Unit) :RecyclerView.Adapter<MyViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding :ListItemBinding = DataBindingUtil.inflate(layoutInflater,
@@ -21,7 +22,7 @@ class MyRecyclerViewAdapter (private  val subscriberList: List<Subscriber>) :Rec
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(subscriberList[position])
+        holder.bind(subscriberList[position],clickListener)
     }
 
     override fun getItemCount(): Int {
@@ -32,10 +33,10 @@ class MyRecyclerViewAdapter (private  val subscriberList: List<Subscriber>) :Rec
 
 
 class MyViewHolder(val binding: ListItemBinding) :RecyclerView.ViewHolder(binding.root){
-    fun  bind(subscriber: Subscriber){
+    fun  bind(subscriber: Subscriber,clickListener: (Subscriber) -> Unit){
         binding.nameTextView.text = subscriber.name
         binding.emailTextView.text = subscriber.email
-
+        binding.listItenLayout.setOnClickListener{clickListener(subscriber)}
 
     }
 }
