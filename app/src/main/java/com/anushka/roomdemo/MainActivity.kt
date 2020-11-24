475bee85bf74f06490f61fe3622e169241760c62
PeteHack <pwachira900@gmail.com>
2020-11-24 11:28:44 +0300

    add view binding and initialize view model

--- app/src/main/java/com/anushka/roomdemo/MainActivity.kt
+++ app/src/main/java/com/anushka/roomdemo/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.anushka.roomdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.anushka.roomdemo.databinding.ActivityMainBinding
+import com.anushka.roomdemo.db.SubscriberDatabase
+import com.anushka.roomdemo.db.SubscriberRepository
+
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var subscriberViewModel: SubscriberViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val dao = SubscriberDatabase.getInstance(application).subscriberDAO
+        val repository = SubscriberRepository(dao)
+        val factory = SubscriberViewModelFactory(repository)
+        subscriberViewModel = ViewModelProvider(this,factory).get(SubscriberViewModel::class.java)
+        binding.myViewModel = subscriberViewModel
+        binding.lifecycleOwner = this
+        displaySubscribersList()
+
+    }
+
+    private fun displaySubscribersList(){
+        subscriberViewModel.subscribers.observe(this, Observer {
+            Log.i("MYTAG",it.toString())
+        })
     }
 }
+
