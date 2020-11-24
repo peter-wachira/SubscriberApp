c87b9a07bf67429e47cfa2788e313dd9496c7879
PeteHack <pwachira900@gmail.com>
2020-11-24 15:36:36 +0300

    modify list item clicked

--- app/src/main/java/com/anushka/roomdemo/ui/MainActivity.kt
+++ app/src/main/java/com/anushka/roomdemo/ui/MainActivity.kt
@@ -54,6 +54,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun listItemClicked(subscriber: Subscriber){
         Toast.makeText(this, "selected name is ${subscriber.name}",Toast.LENGTH_LONG).show()
+        subscriberViewModel.initUpdateAndDelete(subscriber)
     }
 
 }
