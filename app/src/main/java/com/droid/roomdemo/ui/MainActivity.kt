bfec32ab1e0ee1f6b72d2e8aa7c7ce611a7c2b26
PeteHack <pwachira900@gmail.com>
2020-11-25 13:08:19 +0300

    refactor recycler view to improve app efficiency

--- app/src/main/java/com/droid/roomdemo/ui/MainActivity.kt
+++ app/src/main/java/com/droid/roomdemo/ui/MainActivity.kt
@@ -17,6 +17,7 @@ import com.droid.roomdemo.db.SubscriberRepository
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var subscriberViewModel: SubscriberViewModel
+    private lateinit var adapter: MyRecyclerViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -37,14 +38,17 @@ class MainActivity : AppCompatActivity() {
     }
 
    private fun initRecyclerView(){
-    binding.subscriberRecyclerView.layoutManager = LinearLayoutManager(this)
+       binding.subscriberRecyclerView.layoutManager = LinearLayoutManager(this)
+       adapter = MyRecyclerViewAdapter({selectedItem:Subscriber->listItemClicked(selectedItem)})
+       binding.subscriberRecyclerView.adapter = adapter
+
        displaySubscribersList()
    }
 
     private fun displaySubscribersList(){
         subscriberViewModel.subscribers.observe(this, Observer {
-            Log.i("MYTAG",it.toString())
-            binding.subscriberRecyclerView.adapter = MyRecyclerViewAdapter(it,{selectedItem:Subscriber->listItemClicked(selectedItem)})
+            adapter.setList(it)
+            adapter.notifyDataSetChanged()
         })
     }
 
--- app/src/main/java/com/droid/roomdemo/ui/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/droid/roomdemo/ui/MyRecyclerViewAdapter.kt
@@ -9,16 +9,22 @@ import com.anushka.roomdemo.R
 import com.anushka.roomdemo.databinding.ListItemBinding
 import com.droid.roomdemo.db.Subscriber
 
-class MyRecyclerViewAdapter(private val subscribersList: List<Subscriber>
-                            , private val clickListener:(Subscriber)->Unit)
+class MyRecyclerViewAdapter(private val clickListener:(Subscriber)->Unit)
     : RecyclerView.Adapter<MyViewHolder>()
 {
+    private val subscribersList = ArrayList<Subscriber>()
+
+    fun setList(subscribers: List<Subscriber>){
+        subscribersList.clear()
+        subscribersList.addAll(subscribers)
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
       val layoutInflater = LayoutInflater.from(parent.context)
       val binding : ListItemBinding =
           DataBindingUtil.inflate(layoutInflater, R.layout.list_item,parent,false)
       return MyViewHolder(binding)
     }
+
 
     override fun getItemCount(): Int {
        return subscribersList.size
